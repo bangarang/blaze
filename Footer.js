227b4986670bcb5c09627d0a06ea5b6a93c580d2
tipickle <33402515+tipickle@users.noreply.github.com>
2018-07-28 21:40:29 -0400

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,56 @@
+/**
+ * React Static Boilerplate
+ * https://github.com/kriasoft/react-static-boilerplate
+ *
+ * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Link from '../Link';
+import s from './Footer.scss';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import cx from 'classnames';
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "hello@chasegrock.com",
+      copied: false
+    };
+  }
+
+  copy(){
+    this.setState({copied: true})
+  }
+  componentDidMount() {
+    // window.componentHandler.upgradeElement(this.root);
+  }
+
+  componentWillUnmount() {
+    // window.componentHandler.downgradeElements(this.root);
+  }
+
+  render() {
+
+    return (
+        <footer className={s.footer}>
+          <p className={s.subheading}>Chase G Rock</p>
+          <CopyToClipboard
+            text={this.state.value}
+            onCopy={this.copy.bind(this)}>
+            <p className="subheading copy_button"><span className="highlight">hello@chasegrock.com</span><span className={this.state.copied ? "copy_status copied" : "copy_status"}>{this.state.copied ? "Copied, Talk to you soon!": "Click to Copy"}</span></p>
+          </CopyToClipboard>
+          <div className={s.social}>
+            <a className={cx(s.social_icon, "icon-spotify")} href="https://play.spotify.com/user/1252553485" target="_blank"></a>
+          </div>
+        </footer>
+    );
+  }
+
+}
+
+export default Footer;
